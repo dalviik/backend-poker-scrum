9848ac0652e66a5c3062b7e40ff2266c239235c5
dalviik <duglasapazaa@gmail.com>
2021-02-04 07:47:37 -0400

    Add room services

--- models/index.js
+++ models/index.js
@@ -9,6 +9,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.room = require('./room')(sequelize, Sequelize);
-db.player = require('./grupo')(sequelize, Sequelize);
+db.player = require('./player')(sequelize, Sequelize);
 
 module.exports = db;
